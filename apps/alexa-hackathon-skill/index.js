356b8f4520929fe5b5242b4a0a7f9a3bdef09087
borantaylan <borantaylan@hotmail.com>
2017-04-06 01:08:27 +0200

    LodgingBusinness example

--- apps/alexa-hackathon-skill/index.js
+++ apps/alexa-hackathon-skill/index.js
@@ -4,7 +4,6 @@ module.change_code = 1;
 var alexa = require('alexa-app');
 var app = new alexa.app('hackathon-skill');
 var cheerio = require('cheerio');
-var filesystemIO = require('fs');
 
 var moment = require('moment');
 var parser = require("./parser.js");
@@ -12,18 +11,12 @@ var parser = require("./parser.js");
 
 
 app.launch(function(request, response) {
-  var logBusDir = "./data/";
-  filesystemIO.readdir(logBusDir, (fileerror, filenames) => {
-      /*filenames.forEach(filename => {
-        console.log(logBusDir+filename);
 
-        //var tempJSON = parser.parseFile(logBusDir+file);
-        //listOfHotelJSONs.push(tempJSON);
-      });*/
-  });
-  var listOfHotelJSONs = [];
-
+    var listOfHotelJSONs = [];
+    listOfHotelJSONs = parser.parseFile("LodgingBusiness");
+    console.log(listOfHotelJSONs);
     request.getSession().set("listOfHotels", listOfHotelJSONs);
+    console.log(listOfHotelJSONs);
     response.say('Welcome to Voice of Seefeld!').reprompt('You have any requests?').shouldEndSession(false);
 
 });
@@ -81,13 +74,13 @@ app.intent('Information', {
     response.say("I will send you the information on your phone.");
     response.card({
         type: "Standard",
-        title: "Informations about Sporthotel Xander", // this is not required for type Simple or Standard 
+        title: "Informations about Sporthotel Xander", // this is not required for type Simple or Standard
         text: "Description: In the heart of Leutasch the family run **** Hotel Xander, the oldest inn in the valley, is tucked away next to the onion-domed village-church. 150 beds in 12 double-rooms and 55 apartments with lounge area, bath/shower/WC. Telephone, safe, radio, SAT-TV and kitchenette\n dog per day 11,00 €\ngarage 10,00 € per day\nsauna, swiming pool is free\njacuzzi + solarium 7,00 €\nlaundry service 8,00 €\nlocal tax excl. \nlocationplan: the Hotel is in front of the church at the district Kirchplatzl.\nCheck in 14.00  Check out 10.00\nAddress: Kirchplatzl 147 in Leutasch\nTelephone: +4352146581\nWebsite: http://www.xander-leutasch.at",
-        image: { // image is optional 
-            smallImageUrl: "https://resc.deskline.net/images/SEE/1/537c5c99-31af-45dc-943a-9a7fefe315ba/99/image.jpg", // required 
+        image: { // image is optional
+            smallImageUrl: "https://resc.deskline.net/images/SEE/1/537c5c99-31af-45dc-943a-9a7fefe315ba/99/image.jpg", // required
             largeImageUrl: "https://resc.deskline.net/images/SEE/1/6ceb14dd-8f3f-4949-9bb9-947c98bc8350/99/image.jpg"
         }
-    });    
+    });
 });
 
 app.intent('Availability', {
